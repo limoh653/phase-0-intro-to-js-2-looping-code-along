9bff92c1ca7fd2289d59523cac92d5d40b72aa20
limoh653 <limohesbon7@gmail.com>
2024-06-27 23:46:05 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(names,result){
+    const results=[];
+    for (let i=0; i<names.length;i++){
+        const result=(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        results.push(result);
+      }
+      return results;
+      
+}
+writeCards(names);
+
+
+function  countDown (){ 
+    countDown=10;
+while (countDown>=0) {
+  console.log(countDown--);
+}}
